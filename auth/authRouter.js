8c82c6d57d19839f98dba88009ae9ee8d1343ef2
Tom Bauer <tbauer1898@gmail.com>
2020-10-12 17:41:49 -0500

    Done with MVP

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -46,4 +46,20 @@ router.post("/login", (req, res) => {
         })
 });
 
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.status(500).json({message : "Failed to logout please try again"})
+            } else {
+                res.status(204).end();
+            }
+        });
+    } else {
+        res.status(204).end()
+    }
+})
+
+
+
 module.exports = router;
